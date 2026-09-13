3489137b318b8877f2a9f37c45518d443d0c7e8a
Helvi <helvi.lainekallio@robu.fi>
2026-09-13 13:38:00 +0300

    Keep Benben hidden from site navigation

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -103,12 +103,6 @@ export const SECTIONS: NavSection[] = [
     icon: "simulations",
     items: [
       {
-        href: "/benben/",
-        label: (t) => t.benben.title,
-        description: (t) => t.benben.lede,
-        badge: (t) => t.benben.badge,
-      },
-      {
         href: "/simulations/irrigation-planner/",
         label: (t) => t.tools.irrigation.name,
         description: (t) => t.tools.irrigation.description,
